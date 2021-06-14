3ac842991552301d4d0a44f11f112c5961034ee9
Kris Litman <kris.d.litman@gmail.com>
2021-06-13 21:22:52 -0400

    Converted list to ord values

--- classes/bubble_sort.py
+++ classes/bubble_sort.py
@@ -1,2 +1,15 @@
 class BubbleSort:
     list = []
+
+    def sort(self, list):
+        converted = self.convertList(list)
+        import pdb
+        pdb.set_trace()
+        return list
+
+    def convertList(self, list):
+        newList = []
+        for l in list:
+            newList.append(ord(l))
+
+        return newList
--- spec/test_bubble_sort.py
+++ spec/test_bubble_sort.py
@@ -5,3 +5,9 @@ from ..classes.bubble_sort import BubbleSort
 def test_bubble_sort_exists():
     bubble = BubbleSort()
     assert isinstance(bubble, BubbleSort)
+
+
+def test_bubble_sort_can_sort():
+    bubble = BubbleSort()
+    expected = bubble.sort(["d", "b", "a", "c"])
+    assert(expected == ["a", "b", "c", "d"])
