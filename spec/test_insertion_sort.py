14bbf6f7efede8f14940c0f623589010e9026473
Kris Litman <kris.d.litman@gmail.com>
2021-06-13 23:23:12 -0400

    Finished insertion sort

--- spec/test_insertion_sort.py
+++ spec/test_insertion_sort.py
@@ -1,7 +1,14 @@
 import pytest
+from ..classes import converter
 from ..classes.insertion_sort import InsertionSort
 
 
 def test_insertion_sort_exists():
     insertion = InsertionSort()
     assert isinstance(insertion, InsertionSort)
+
+
+def test_insertion_sort_sorts():
+    insertion = InsertionSort()
+    expected = insertion.sort(["d", "b", "a", "c"])
+    assert(expected == ["a", "b", "c", "d"])
