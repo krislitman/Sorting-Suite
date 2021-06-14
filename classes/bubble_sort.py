a217b3c8683c5b56da4a293b3d1a3dd7f869889e
Kris Litman <kris.d.litman@gmail.com>
2021-06-13 21:38:39 -0400

    Bubble Sort can do the thing

--- classes/bubble_sort.py
+++ classes/bubble_sort.py
@@ -3,13 +3,27 @@ class BubbleSort:
 
     def sort(self, list):
         converted = self.convertList(list)
-        import pdb
-        pdb.set_trace()
-        return list
+        length = len(converted) - 1
+        for c in converted:
+            for l in range(length):
+                if converted[l] > converted[l + 1]:
+                    value = converted[l]
+                    converted[l] = converted[l + 1]
+                    converted[l + 1] = value
+
+        expected = self.convertBack(converted)
+        return expected
 
     def convertList(self, list):
         newList = []
         for l in list:
             newList.append(ord(l))
+
+        return newList
+
+    def convertBack(self, list):
+        newList = []
+        for l in list:
+            newList.append(chr(l))
 
         return newList
--- spec/test_bubble_sort.py
+++ spec/test_bubble_sort.py
@@ -7,6 +7,18 @@ def test_bubble_sort_exists():
     assert isinstance(bubble, BubbleSort)
 
 
+def test_bubble_sort_can_convert_values():
+    bubble = BubbleSort()
+    converted = bubble.convertList(["d", "b", "a", "c"])
+    assert(converted == [100, 98, 97, 99])
+
+
+def test_bubble_sort_can_convert_values_back():
+    bubble = BubbleSort()
+    converted = bubble.convertBack([100, 98, 97, 99])
+    assert(converted == ["d", "b", "a", "c"])
+
+
 def test_bubble_sort_can_sort():
     bubble = BubbleSort()
     expected = bubble.sort(["d", "b", "a", "c"])
